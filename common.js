5e5dc1492435a35dbcad4554f87654b4d4af2d84
cantimagine <abramk3lly@gmail.com>
2018-02-21 19:30:04 -0500

    added jquery, changed pubs page (in mostly invisible ways)

---
+++ common.js
@@ -0,0 +1,20 @@
+
+
+$(function(){
+    // add hover text on NewWindow links
+    $("a.newwnd").attr("title", "Opens in New Window");
+
+    // change style of current nav item
+    var currentPage = getCurrentFileName();
+    $("nav li a").each(function(){
+        if ($(this).attr("href") == currentPage) {
+            $(this).parent().addClass("current");
+            $(this).attr("href", "javascript:;").css("cursor", "default");
+        }
+    });
+});
+
+function getCurrentFileName(){
+    var pagePathName= window.location.pathname;
+    return pagePathName.substring(pagePathName.lastIndexOf("/") + 1);
+}
